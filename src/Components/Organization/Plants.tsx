7cfd05e5821f8a0579ad054822925483ceee31da
rahulbulfro <rahul@bulfro.com>
2025-08-26 17:44:53 +0530

    Remove unnecessary margin from Plants table: Adjust table class to eliminate bottom margin, enhancing layout consistency in the Plants view.

--- src/Components/Organization/Plants.tsx
+++ src/Components/Organization/Plants.tsx
@@ -298,7 +298,7 @@ const Plants = () => {
         </div>
       ) : (
         <div className="relative overflow-auto shadow-sm rounded-lg pb-0 bg-primary flex-1">
-          <table className="w-full text-sm text-left rtl:text-right text-text-primary mb-16">
+          <table className="w-full text-sm text-left rtl:text-right text-text-primary">
             <thead className="text-xs text-text-primary uppercase bg-primary border-b border-border-primary sticky top-0 z-10">
               <tr>
                 <th className="p-4 text-text-primary whitespace-nowrap text-center text-base font-roboto font-medium">
