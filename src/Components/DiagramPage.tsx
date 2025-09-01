1f82ed894310b183224573351698ca51ea6db059
rahulbulfro <rahul@bulfro.com>
2025-09-01 18:25:24 +0530

    Enhance DiagramPage with initial nodes and edges setup: Introduce initial node and edge configurations for diagram representation, enabling a more interactive and structured diagram editing experience using React Flow.

--- src/Components/DiagramPage.tsx
+++ src/Components/DiagramPage.tsx
@@ -1,9 +1,108 @@
 import { ChevronsLeft } from "lucide-react";
 import { useNavigate } from "react-router-dom";
-import ReactFlow, { Background, BackgroundVariant } from "reactflow";
+import ReactFlow, {
+  Background,
+  BackgroundVariant,
+  useEdgesState,
+  useNodesState,
+} from "reactflow";
 import "reactflow/dist/style.css";
 
+const initialNodes = [
+  {
+    id: "1",
+    data: { label: "Group A", type: "output" },
+    position: { x: 50, y: 50 },
+    style: { width: 700, height: 350, borderRadius: 10 },
+    type: "group",
+  },
+  {
+    id: "fm1",
+    data: { label: "FM1", type: "output" },
+    position: { x: 20, y: 20 },
+    parentId: "1",
+    sourcePosition: "right",
+    targetPosition: "right",
+  },
+  {
+    id: "fm2",
+    data: { label: "FM2", type: "output" },
+    position: { x: 20, y: 270 },
+    parentId: "1",
+    sourcePosition: "right",
+    targetPosition: "right",
+  },
+  {
+    id: "tank1",
+    data: { label: "Tank", type: "bidirectional" },
+    position: { x: 280, y: 150 },
+    parentId: "1",
+    targetPosition: "left",
+    sourcePosition: "right",
+  },
+  {
+    id: "fm3",
+    data: { label: "FM3", type: "input" },
+    position: { x: 500, y: 20 },
+    parentId: "1",
+    sourcePosition: "right",
+    targetPosition: "left",
+  },
+  {
+    id: "fm4",
+    data: { label: "FM4", type: "input" },
+    position: { x: 500, y: 270 },
+    parentId: "1",
+    sourcePosition: "right",
+    targetPosition: "left",
+  },
+  {
+    id: "2",
+    data: { label: "Group B", type: "output" },
+    position: { x: 800, y: 50 },
+    style: { width: 700, height: 350, borderRadius: 10 },
+    type: "group",
+  },
+  {
+    id: "fm5",
+    data: { label: "FM5", type: "input" },
+    position: { x: 20, y: 150 },
+    parentId: "2",
+    sourcePosition: "right",
+    targetPosition: "left",
+  },
+  {
+    id: "fm6",
+    data: { label: "FM6", type: "input" },
+    position: { x: 500, y: 150 },
+    parentId: "2",
+    sourcePosition: "right",
+    targetPosition: "left",
+  },
+  {
+    id: "tank2",
+    data: { label: "Tank", type: "bidirectional" },
+    position: { x: 270, y: 150 },
+    parentId: "2",
+    sourcePosition: "right",
+    targetPosition: "left",
+  },
+];
+
+const initialEdges = [
+  { id: "fm1-tank1", source: "fm1", target: "tank1", animated: true },
+  { id: "fm2-tank1", source: "fm2", target: "tank1", animated: true },
+  { id: "tank1-fm3", source: "tank1", target: "fm3", animated: true },
+  { id: "tank1-fm4", source: "tank1", target: "fm4", animated: true },
+  { id: "fm4-fm5", source: "fm4", target: "fm5", animated: true },
+  { id: "fm5-tank2", source: "fm5", target: "tank2", animated: true },
+  { id: "tank2-fm6", source: "tank2", target: "fm6", animated: true },
+];
+
 const DiagramPage = () => {
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
   const navigate = useNavigate();
 
   const handleBack = () => {
@@ -24,7 +123,13 @@ const DiagramPage = () => {
             </button>
           </div>
           <div className="h-full w-full p-4">
-            <ReactFlow className="h-full w-full">
+            <ReactFlow
+              className="h-full w-full"
+              nodes={nodes}
+              edges={edges}
+              onNodesChange={onNodesChange}
+              onEdgesChange={onEdgesChange}
+            >
               <Background variant={BackgroundVariant.Dots} />
             </ReactFlow>
           </div>
