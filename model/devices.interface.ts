ec04e48f402844e1b5261cbd534696839fa3d286
rahulbulfro <rahul@bulfro.com>
2025-09-05 19:03:07 +0530

    Update DeviceResult interface: Add last_record_time, last_record, params, and additional properties for enhanced device data representation and management.

--- model/devices.interface.ts
+++ model/devices.interface.ts
@@ -15,8 +15,42 @@ export interface DeviceResult {
   department_id: number;
   created_at: string;
   updated_at: string;
-  device_family: string;
-  device_type: string;
+  last_record_time: string;
+  last_record: LastRecord;
+  params: Params;
+  name: string;
+  device_type_name: string;
   department_name: string;
   project_name: string;
+  latitude: string;
+  longitude: string;
+  address: string;
+}
+
+export interface LastRecord {
+  hrs_max?: number;
+  hrs_min?: number;
+  min_avg?: number;
+  min_max?: number;
+  min_min?: number;
+  min_flow?: number;
+  day_max_level?: number;
+  day_min_level?: number;
+  hrs_max_level?: number;
+  hrs_min_level?: number;
+  min_max_level?: number;
+  min_min_level?: number;
+  day_last_level?: number;
+  hrs_last_level?: number;
+  min_last_level?: string;
+  hrs_first_level?: number;
+  min_first_level?: string;
+}
+
+export interface Params {
+  height?: number;
+  shifter?: number;
+  multiplier?: number;
+  sensorPostion?: number;
+  storageCapacity?: number;
 }
