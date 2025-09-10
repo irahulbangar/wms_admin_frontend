0c356197b6898d422ba686bbcbb330155b901ecd
rahulbulfro <rahul@bulfro.com>
2025-09-10 17:11:42 +0530

    Comment out the organization settings menu item in the Sidebar component to streamline the menu options and improve clarity.

--- src/Components/Layout/Sidebar.tsx
+++ src/Components/Layout/Sidebar.tsx
@@ -56,12 +56,12 @@ const menuItems = [
         icon: <User className="w-5 h-5" />,
         href: "/organization/users",
       },
-      {
-        id: "organization-setting",
-        label: "Setting",
-        icon: <Settings className="w-5 h-5" />,
-        href: "/organization/setting",
-      },
+      // {
+      //   id: "organization-setting",
+      //   label: "Setting",
+      //   icon: <Settings className="w-5 h-5" />,
+      //   href: "/organization/setting",
+      // },
     ],
   },
   {
