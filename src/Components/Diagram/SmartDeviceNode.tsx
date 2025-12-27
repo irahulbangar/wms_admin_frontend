577499b21e6de62daa930c575b72cdd315a272e6
Rahul Bangar <rahul@bulfro.com>
2025-12-27 16:53:01 +0530

    feat: Filter display parameters in SmartDeviceNode for improved visibility, removing unused code for cleaner implementation.

--- src/Components/Diagram/SmartDeviceNode.tsx
+++ src/Components/Diagram/SmartDeviceNode.tsx
@@ -17,7 +17,10 @@ const SmartDeviceNode: React.FC<SmartDeviceNodeProps> = ({ data }) => {
   const systemConnection = data.systemConnection || "";
   const deviceName = data.label || "";
   const lastRecordTime = data.lastRecordTime || "";
-  const displayParams = data.displayParams || [];
+  const displayParams =
+    data.displayParams?.filter(
+      (param) => param.diagram_visible === 1 && param.report_visible !== 0
+    ) || [];
   const recordTimeOld = isRecordTimeOld(lastRecordTime);
 
   const connectionInfo = [
@@ -52,22 +55,6 @@ const SmartDeviceNode: React.FC<SmartDeviceNodeProps> = ({ data }) => {
           alt="smart-device"
           className="w-12 h-12 object-contain"
         />
-        {/* <div className="w-12 h-12 flex items-center justify-center bg-status-info/20 rounded-md">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="w-8 h-8 text-status-info"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
-            />
-          </svg>
-        </div> */}
       </div>
 
       <div className="flex flex-col mt-1">
@@ -76,7 +63,8 @@ const SmartDeviceNode: React.FC<SmartDeviceNodeProps> = ({ data }) => {
             key={param.name}
             className="text-sm font-normal text-left font-roboto text-wrap px-1 text-text-primary leading-4 whitespace-nowrap"
           >
-            {param.display_name} : <span className="italic text-xs">({param.unit})</span>
+            {param.name} :{" "}
+            <span className="italic text-xs">({param.unit})</span>
           </div>
         ))}
       </div>
